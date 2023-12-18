b10fcaf7833dfccec9f565153c26833daa550e87
Pompeu Costa <pompeu@ua.pt>
2023-12-18 11:42:20 +0000

    fix2

--- src/App.js
+++ src/App.js
@@ -6,23 +6,9 @@ import "swagger-ui-react/swagger-ui.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <SwaggerUI url="/webApi/swagger.json" />
+      </div>
   );
 }
 
-export default App = () => <SwaggerUI url="/webApi/swagger.json" />
-//export default App;
+export default App;
